f8dcc16fe09347cc35d8a75cc843b286712ea6ff
tandat56 <datlt,letandat@gmail.com>
2020-11-28 00:22:30 +0700

    load category product and product to products page

--- src/main/java/com/souvenironline/service/admin/IBlogAdminService.java
+++ src/main/java/com/souvenironline/service/admin/IBlogAdminService.java
@@ -6,10 +6,49 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
+/**
+ * The interface Blog admin service.
+ */
 public interface IBlogAdminService {
+
+
+    /**
+     * Find all list.
+     *
+     * @param pageable the pageable
+     * @return the list
+     */
     List<BlogDTO> findAll(Pageable pageable);
+
+
+    /**
+     * Gets total item.
+     *
+     * @return the total item
+     */
     int getTotalItem();
-    BlogDTO findById(long id);
+
+
+    /**
+     * Find by id blog dto.
+     *
+     * @param idOfBlog the id of blog
+     * @return the blog dto
+     */
+    BlogDTO findById(long idOfBlog);
+
+    /**
+     * Save blog dto.
+     *
+     * @param dto the dto
+     * @return the blog dto
+     */
     BlogDTO save(BlogDTO dto);
+
+    /**
+     * Delete.
+     *
+     * @param ids the ids
+     */
     void delete(long[] ids);
 }
